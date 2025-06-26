87187b278d2ba795aef1ed78d46fca9c5c194eb6
Nico Pareigis <nico@pareigis.de>
2025-06-26 10:38:35 +0200

    refactor: style

--- main.go
+++ main.go
@@ -12,44 +12,37 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var userStatMap = make(map[string]map[string]float64)
+type Stats = map[string]float64
+type User = string
+
+var userStats = make(map[User]Stats)
 
 // Listens to messages sent in the result-spam channel
 func onMessageCreate(session *discordgo.Session, message *discordgo.MessageCreate) {
 	if strings.HasPrefix(message.Content, "stats") {
 		sendDailyStats(session, message.ChannelID)
 		return
-	}
-	if message.ChannelID != getChannelIDByName(session, message.GuildID, "result-spam") {
+	} else if message.ChannelID != getChannelIDByName(session, message.GuildID, "result-spam") {
 		return
-	}
-	if !strings.HasPrefix(message.Content, "I've completed all the modes of #LoLdle today:") {
+	} else if !strings.HasPrefix(message.Content, "I've completed all the modes of #LoLdle today:") {
 		return
 	}
 
-	lines := strings.Split(message.Content, "\n")
-	stats := make(map[string]float64)
-	for i, line := range lines {
-		if i == 0 {
-			continue
-		}
-
+	stats := make(Stats)
+	for _, ln := range strings.Split(message.Content, "\n")[1:] {
 		// remove emoji
-		lineParts := strings.SplitN(line, " ", 2)
-		if len(lineParts) < 2 {
+		words := strings.SplitN(ln, " ", 2)
+		if len(words) < 2 {
 			continue
 		}
-
-		remainingLine := lineParts[1]
 
 		// split key and value
-		dataParts := strings.SplitN(remainingLine, ":", 2)
-		if len(dataParts) < 2 {
+		data := strings.SplitN(words[1], ":", 2)
+		if len(data) < 2 {
 			continue
 		}
 
-		key := strings.TrimSpace(dataParts[0])
-		value := strings.TrimSpace(dataParts[1])
+		key, value := strings.TrimSpace(data[0]), strings.TrimSpace(data[1])
 
 		isChecked := strings.HasSuffix(value, "✓")
 		if isChecked {
@@ -71,53 +64,52 @@ func onMessageCreate(session *discordgo.Session, message *discordgo.MessageCreat
 		stats[key] = floatValue
 	}
 
-	userStatMap[message.Author.ID] = stats
+	userStats[message.Author.ID] = stats
 }
 
 // Schedules tasks to run at midnight
 func scheduleMidnight(session *discordgo.Session, channelID string) {
-	go func() {
-		for {
-			now := time.Now()
-			nextMidnight := time.Date(
-				now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location(),
-			)
-			durationUntilNextMidnight := time.Until(nextMidnight)
-			time.Sleep(durationUntilNextMidnight)
+	for {
+		now := time.Now()
+		nextMidnight := time.Date(
+			now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location(),
+		)
+		durationUntilNextMidnight := time.Until(nextMidnight)
+		time.Sleep(durationUntilNextMidnight)
 
-			// Do this at midnight
-			// Todo: generate a message that displays the "elo" of all tracked users
-			sendDailyStats(session, channelID)
-		}
-	}()
+		// Do this at midnight
+		// TODO: generate a message that displays the "elo" of all tracked users
+		sendDailyStats(session, channelID)
+	}
 }
 
 func sendDailyStats(session *discordgo.Session, channelID string) {
-	// Todo: make this nice
+	// TODO: make this nice
 
-	var output = ""
-	for userID, stats := range userStatMap {
+	str := ""
+	for userID, stats := range userStats {
 		user, err := session.GuildMember("1387198610935906305", userID)
 		if err != nil {
 			log.Printf("Failed get username: %v", err)
 		}
 
-		output += user.DisplayName() + ":\n"
+		str += user.DisplayName() + ":\n"
 		for key, value := range stats {
-			output += fmt.Sprintf("%s: %g\n", key, value)
+			str += fmt.Sprintf("%s: %g\n", key, value)
 		}
-		output += "\n"
+		str += "\n"
 	}
-	sendMessage(session, channelID, output)
+	sendMessage(session, channelID, str)
 }
 
 func sendMessage(session *discordgo.Session, channelID string, content string) {
 	_, err := session.ChannelMessageSend(channelID, content)
 	if err != nil {
 		log.Printf("Failed to send message: %v", err)
-	} else {
-		log.Printf("Message sent to channel %s", channelID)
+		return
 	}
+
+	log.Printf("Message sent to channel %s", channelID)
 }
 
 func getChannelIDByName(session *discordgo.Session, serverID string, channelName string) string {
@@ -127,9 +119,9 @@ func getChannelIDByName(session *discordgo.Session, serverID string, channelName
 		return ""
 	}
 
-	for _, channel := range channels {
-		if channel.Name == channelName {
-			return channel.ID
+	for _, ch := range channels {
+		if ch.Name == channelName {
+			return ch.ID
 		}
 	}
 
@@ -151,8 +143,7 @@ func main() {
 	// create a new session
 	session, err := discordgo.New("Bot " + token)
 	if err != nil {
-		log.Println("Failed to create Discord session,", err)
-		return
+		log.Fatal("Failed to create Discord session", err)
 	}
 
 	// register event handlers
@@ -161,12 +152,11 @@ func main() {
 	// open web socket connection
 	err = session.Open()
 	if err != nil {
-		log.Println("Failed to open the connection,", err)
-		return
+		log.Fatal("Failed to open connection", err)
 	}
 
-	// hardcoded serverID for now
-	scheduleMidnight(session, getChannelIDByName(session, "1387198610935906305", "result-spam"))
+	// TODO: don't hardcode server ID
+	go scheduleMidnight(session, getChannelIDByName(session, "1387198610935906305", "result-spam"))
 
 	fmt.Println("Bot is running...")
 	select {}
