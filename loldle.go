c568c9e5f0275fe15b6661859596f1b8e7d1a459
Nico Pareigis <nico@pareigis.de>
2025-07-21 22:19:40 +0200

    refactor(stats): move CalculateElo to method

--- loldle.go
+++ loldle.go
@@ -31,6 +31,78 @@ type LoldleStats struct {
 	SplashCheck  bool
 }
 
+// CalculateElo calculates the change in user Elo resulting from the given
+// stats. Elo gains are based on the distribution table below, where columns
+// correspond to the number of guesses for each category.
+//
+//	|         |  1 |  2 |  3 |  4 |  5 |
+//	| ------- | -: | -: | -: | -: | -: |
+//	| Classic | +4 | +4 | +2 | -2 | -4 |
+//	| Quote   | +4 | +2 | -2 | -4 |    |
+//	| Ability | +4 | -2 | -4 |    |    |
+//	| Emoji   | +4 | +2 | -2 | -4 |    |
+//	| Splash  | +4 | +2 | -2 | -4 |    |
+//
+// Categories with more guesses than listed net -4 Elo each.
+// Guessing the ability or splash art correctly nets +2 Elo each.
+func (l *LoldleStats) CalculateElo() int {
+	var elo int
+
+	// Classic
+	if l.Classic <= 5 {
+		elo += 4
+		if l.Classic > 2 { // "grace"-guess requires guard
+			elo -= 2 * (l.Classic - 1)
+		}
+	} else {
+		elo -= 4
+	}
+
+	// Quote
+	if l.Quote <= 4 {
+		elo += 4
+		elo -= 2 * (l.Quote - 1)
+	} else {
+		elo -= 4
+	}
+
+	// Ability
+	switch l.Ability {
+	case 1:
+		elo += 4
+	case 2:
+		elo -= 2
+	default:
+		elo -= 4
+	}
+
+	// Emoji
+	if l.Emoji <= 4 {
+		elo += 4
+		elo -= 2 * (l.Emoji - 1)
+	} else {
+		elo -= 4
+	}
+
+	// Splash
+	if l.Splash <= 4 {
+		elo += 4
+		elo -= 2 * (l.Splash - 1)
+	} else {
+		elo -= 4
+	}
+
+	// Checkmarks
+	if l.AbilityCheck {
+		elo += 2
+	}
+	if l.SplashCheck {
+		elo += 2
+	}
+
+	return elo
+}
+
 // CanParse reports whether the given message may be parsed into [LoldleStats].
 //
 // This function only performs baseline validation, such that parsing for a
--- stats.go
+++ stats.go
@@ -7,78 +7,6 @@ import (
 	"unsafe"
 )
 
-// CalculateElo calculates the change in user Elo resulting from the given
-// stats. Elo gains are based on the distribution table below, where columns
-// correspond to the number of guesses for each category.
-//
-//	|         |  1 |  2 |  3 |  4 |  5 |
-//	| ------- | -: | -: | -: | -: | -: |
-//	| Classic | +4 | +4 | +2 | -2 | -4 |
-//	| Quote   | +4 | +2 | -2 | -4 |    |
-//	| Ability | +4 | -2 | -4 |    |    |
-//	| Emoji   | +4 | +2 | -2 | -4 |    |
-//	| Splash  | +4 | +2 | -2 | -4 |    |
-//
-// Categories with more guesses than listed net -4 Elo each.
-// Guessing the ability or splash art correctly nets +2 Elo each.
-func CalculateElo(l *LoldleStats) int {
-	var elo int
-
-	// Classic
-	if l.Classic <= 5 {
-		elo += 4
-		if l.Classic > 2 { // "grace"-guess requires guard
-			elo -= 2 * (l.Classic - 1)
-		}
-	} else {
-		elo -= 4
-	}
-
-	// Quote
-	if l.Quote <= 4 {
-		elo += 4
-		elo -= 2 * (l.Quote - 1)
-	} else {
-		elo -= 4
-	}
-
-	// Ability
-	switch l.Ability {
-	case 1:
-		elo += 4
-	case 2:
-		elo -= 2
-	default:
-		elo -= 4
-	}
-
-	// Emoji
-	if l.Emoji <= 4 {
-		elo += 4
-		elo -= 2 * (l.Emoji - 1)
-	} else {
-		elo -= 4
-	}
-
-	// Splash
-	if l.Splash <= 4 {
-		elo += 4
-		elo -= 2 * (l.Splash - 1)
-	} else {
-		elo -= 4
-	}
-
-	// Checkmarks
-	if l.AbilityCheck {
-		elo += 2
-	}
-	if l.SplashCheck {
-		elo += 2
-	}
-
-	return elo
-}
-
 // DailyStats contains a summary of a single game of LoLdle (see [LoldleStats]).
 // This additionally includes the ID of the user who posted the stats as well as
 // the change in Elo resulting from the stats.
@@ -109,7 +37,7 @@ func NewDailyStats(uID string, l *LoldleStats) *DailyStats {
 		Splash:       l.Splash,
 		SplashCheck:  l.SplashCheck,
 
-		EloChange: CalculateElo(l),
+		EloChange: l.CalculateElo(),
 	}
 }
 
